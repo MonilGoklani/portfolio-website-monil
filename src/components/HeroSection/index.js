97bd2b6d4c742aef1b712e0abfe01c78b9f9cbe9
MonilGoklani <monil2912@gmail.com>
2021-06-21 22:37:20 -0500

    added hero elements

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Video from '../../videos/video.mp4'
+import {HeroContainer,HeroBg,VideoBg, HeroContent, HeroH1, HeroP, HeroBtnWrapper, ArrowForward, ArrowRight} from './HeroElements'
 
 const HeroSection = () => {
     return (
+    const [hover, setHover] = useState(false);
+
+    const onHover = () => {
+        setHover(!hover)
+    }
+
+
         <HeroContainer>
             <HeroBg>
-                <VideoBg autoplay loop muted src = {Video} type='video/mp4'></VideoBg>
+                <VideoBg autoPlay loop muted src={Video} type='video/mp4'/>
             </HeroBg>
+            <HeroContent>
+                <HeroH1>Virtual Banking Made Easy</HeroH1>
+                <HeroP>
+                    Sign up for a new account today and receive $250 in credit towards your next payment
+                </HeroP>
+                <HeroBtnWrapper>
+                    <Button to ='signup' onMouseEnter={onHover} onMouseLeave={onHover}>
+                        Get started {hover ? <ArrowForward /> : <ArrowRight/>}
+                    </Button>
+                </HeroBtnWrapper>
+            </HeroContent>
         </HeroContainer>
     )
 }
