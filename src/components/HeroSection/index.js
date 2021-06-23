1007065e942f1af464cf71934975c398f59a23bc
MonilGoklani <monil2912@gmail.com>
2021-06-23 17:32:22 -0500

    added HeroElements Styling

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,16 +1,14 @@
 import React, {useState} from 'react'
 import Video from '../../videos/video.mp4'
 import {HeroContainer,HeroBg,VideoBg, HeroContent, HeroH1, HeroP, HeroBtnWrapper, ArrowForward, ArrowRight} from './HeroElements'
-
+import {Button} from '../ButtonElement'
 const HeroSection = () => {
-    return (
     const [hover, setHover] = useState(false);
 
     const onHover = () => {
         setHover(!hover)
     }
-
-
+    return (
         <HeroContainer>
             <HeroBg>
                 <VideoBg autoPlay loop muted src={Video} type='video/mp4'/>
@@ -21,7 +19,7 @@ const HeroSection = () => {
                     Sign up for a new account today and receive $250 in credit towards your next payment
                 </HeroP>
                 <HeroBtnWrapper>
-                    <Button to ='signup' onMouseEnter={onHover} onMouseLeave={onHover}>
+                    <Button to='signup' onMouseEnter={onHover} onMouseLeave={onHover} primary='true' dark='true'>
                         Get started {hover ? <ArrowForward /> : <ArrowRight/>}
                     </Button>
                 </HeroBtnWrapper>
