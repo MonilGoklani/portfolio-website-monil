3ba56de3f34a39e5a82d8d26fb7f61ec967e56b3
MonilGoklani <monil2912@gmail.com>
2021-07-05 13:54:01 -0500

    fixed hamburger menu

--- src/components/HamburgerMenu/SidebarElements.js
+++ src/components/HamburgerMenu/SidebarElements.js
@@ -40,11 +40,11 @@ export const SidebarWrapper = styled.div`
 export const SidebarMenu = styled.ul`
 display: grid;
 grid-template-columns: 1fr;
-grid-template-rows: repeat(6, 80px);
+grid-template-rows: repeat(4, 80px);
 text-align: center;
 
 @media screen and (max-width: 400px) {
-    grid-template-rows: repeat(6, 60px);
+    grid-template-rows: repeat(4, 60px);
 }
 `
 
